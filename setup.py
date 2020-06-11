7be65583fb12a88ba3879b57c264ad1dea9d6eec
zhaojinhui <bbxxone@qq.com>
2020-06-11 22:48:57 +0800

    Try to update version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="piece",
-    version="0.0.1",
+    version="0.0.2",
     author="redfoxatasleep",
     author_email="bbxxone@qq.com",
     description="Useful Code Piece.",

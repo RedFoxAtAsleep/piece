92db47b4026df0e9c6026f54af457576730fa3ba
zhaojinhui <bbxxone@qq.com>
2020-07-13 16:56:29 +0800

    Synchronize Before Publish.

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ git commit -m 'Synchronize Before Publish.'
 git push
 
 rm -rf dist
+python3 setup.py sdist bdist_wheel
 python3 -m twine upload --repository pypi dist/*
 pip uninstall piece3941
 pip install piece3941 -i http://mirrors.aliyun.com/pypi/simple/
--- publish_test.sh
+++ publish_test.sh
@@ -4,6 +4,8 @@ git commit -m 'Synchronize Before Publish.'
 git push
 
 rm -rf dist
+
+python3 setup.py sdist bdist_wheel
 python3 -m twine upload --repository testpypi dist/*
 pip uninstall piece3941
 pip install -i https://test.pypi.org/simple/ piece3941

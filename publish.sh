97302bcc402eb1885f7f15bae75ee7c364eb2f09
zhaojinhui <bbxxone@qq.com>
2020-07-13 16:59:27 +0800

    Update publish shell.

--- publish.sh
+++ publish.sh
@@ -7,8 +7,6 @@ git push
 rm -rf dist
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload --repository pypi dist/*
-pip uninstall piece3941
-pip install piece3941 -i http://mirrors.aliyun.com/pypi/simple/
 #python3 -m twine upload --repository testpypi dist/*
 #pip uninstall piece3941
 #pip install -i https://test.pypi.org/simple/ piece3941
--- publish_test.sh
+++ publish_test.sh
@@ -7,5 +7,5 @@ rm -rf dist
 
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload --repository testpypi dist/*
-pip uninstall piece3941
-pip install -i https://test.pypi.org/simple/ piece3941
+# pip uninstall piece3941
+# pip install -i https://test.pypi.org/simple/ piece3941
